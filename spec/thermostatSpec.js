c08dbe05180e27edd06346fc8e5e4bd9decbd1d1
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-05-10 15:04:55 +0100

    Adds up and down functions

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,12 +1,39 @@
 describe('Thermostat', function() {
   var thermostat;
+
   beforeEach(function() {
     thermostat = new Thermostat();
   });
 
   describe('#temperature', function() {
-    it('displays default temperature', function() {
+
+    it('returns default temperature', function() {
       expect(thermostat.temperature()).toEqual(20)
+    });
+  });
+
+  describe('#up', function() {
+
+    it('increases temperature by 1', function() {
+      thermostat.up()
+      expect(thermostat.temperature()).toEqual(21)
+    });
+  });
+
+  describe('#down', function() {
+
+    it('decreases temperature by 1', function() {
+      thermostat.down()
+      expect(thermostat.temperature()).toEqual(19)
+    });
+
+    describe('when temperature is 10', function() {
+
+      it('throws error message', function() {
+        spyOn(thermostat, "temperature").and.returnValue(10)
+        expect(function(){ thermostat.down() } ).toThrow("can not decreases temperature below 10")
+      });
+      
     });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,25 @@
 var Thermostat = function(){
   this._degrees = 20
 };
-Thermostat.prototype.temperature = function() {
-  return this._degrees;
+
+Thermostat.prototype = {
+
+  temperature: function() {
+    return this._degrees;
+  },
+
+  up: function() {
+    this._degrees++
+  },
+
+  down: function() {
+    this._degrees--
+    if (this._degrees >== 10) {
+      throw 'can not decrease temperature below 10';
+    }
+    else {
+      return  this._planes.push(plane);
+    }
+  }
+
 };
