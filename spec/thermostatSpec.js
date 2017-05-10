e562bb1d582f5d33c17bc677b368e2f90d016f15
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-05-10 16:28:13 +0100

    Adds maximum temeperature functionality

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -18,6 +18,27 @@ describe('Thermostat', function() {
       thermostat.up()
       expect(thermostat.temperature()).toEqual(21)
     });
+
+    describe('when power save is on', function() {
+      it('it does not increase temperature above 25', function() {
+        var times = 5;
+        for(var i=0; i < times; i++){
+          thermostat.up();
+        };
+        expect(function(){ thermostat.up() } ).toThrow("can not increase temperature above 25 when power saver is on")
+      });
+    });
+
+    describe('when power save is off', function() {
+      it('it does not increase temperature above 32', function() {
+        thermostat.powerSaverOff()
+        var times = 12;
+        for(var i=0; i < times; i++){
+          thermostat.up();
+        };
+        expect(function(){ thermostat.up() } ).toThrow("can not increase temperature above 32")
+      });
+    });
   });
 
   describe('#down', function() {
@@ -30,10 +51,32 @@ describe('Thermostat', function() {
     describe('when temperature is 10', function() {
 
       it('throws error message', function() {
-        spyOn(thermostat, "temperature").and.returnValue(10)
+        var times = 10;
+        for(var i=0; i < times; i++){
+          thermostat.down();
+        };
         expect(function(){ thermostat.down() } ).toThrow("can not decreases temperature below 10")
       });
-      
+
+    });
+
+  });
+
+  describe('#powerSaverOff', function() {
+
+    it('sets power saver mode to false', function() {
+      thermostat.powerSaverOff()
+      expect(thermostat.isPowerSaver()).toEqual(false)
     });
   });
+
+  describe('#powerSaverOn', function() {
+
+    it('sets power saver mode to true again', function() {
+      thermostat.powerSaverOff()
+      thermostat.powerSaverOn()
+      expect(thermostat.isPowerSaver()).toEqual(true)
+    });
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,6 @@
 var Thermostat = function(){
-  this._degrees = 20
+  this._degrees = 20,
+  this._powerSaver = true;
 };
 
 Thermostat.prototype = {
@@ -9,15 +10,35 @@ Thermostat.prototype = {
   },
 
   up: function() {
-    this._degrees++
+    if (this._degrees >= 32 && !this.isPowerSaver())  {
+      throw 'can not increase temperature above 32';
+    }
+    if (this._degrees >= 25 && this.isPowerSaver())  {
+      throw 'can not increase temperature above 25 when power saver is on';
+    }
+    else {
+      this._degrees++
+    }
   },
 
   down: function() {
-    if (this._degrees <== 10) {
-      throw 'can not decrease temperature below 10';
+    if (this._degrees <= 10) {
+      throw 'can not decreases temperature below 10';
     }
     else {
-      this._degrees--;
+      this._degrees--
     }
+  },
+
+  isPowerSaver: function() {
+    return this._powerSaver;
+  },
+
+  powerSaverOff: function() {
+    this._powerSaver = false
+  },
+
+  powerSaverOn: function() {
+    this._powerSaver = true
   }
 };
